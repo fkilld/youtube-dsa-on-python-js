e1549471181c4dc9cb7fc5898344e2cd6ea21852
fkilld <fkill.d.azadd@gmail.com>
2025-02-09 22:45:55 +0530

    Add functions for greatest numbers, leap year check, and prime number detection in JavaScript and Python

--- app.js
+++ app.js
@@ -24,3 +24,66 @@
 //     return sum
 // }
 // console.log(sumOfFirst(10))
+
+// # Greatest of two numbers: js | Python
+// function greatest_of_two(a,b){
+//     if (a>b){
+//         return 'a is greater'
+//     }else {
+
+//         return 'a is greater'
+//     }
+// }
+// console.log(greatest_of_two(10, 5))
+// # Greatest of the Three numbers: js | Python
+// function greatest_of_three(a,b,c){
+//     if ( a>b && a>c  )    return 'a is greater'
+//     else if ( b>a && b>c  )    return 'b is greater'
+//     else return 'c is greater'
+
+// }
+// console.log(greatest_of_three(5,10,12))
+// # Leap year or not: js | Python
+
+// function is_leap_year(year) {
+//     if ((year % 400==0) || (year % 4 == 0 && year % 100 != 0)) {
+//          return ` ${year} is a leap year`
+//     } else {
+//         return ` ${year} is not  a leap year`
+//     }
+// }
+// console.log(is_leap_year(2018))
+//  Prime number: js | Python
+// function prime_number(n) {
+//   if (n <= 1) return false
+//   let i = 2,
+//     flag = true
+//   while (i * i < n) {
+//     if (n % i == 0) {
+//       return false
+//     }
+//     i += 1
+//   }
+//   return flag
+// }
+// console.log(prime_number(15))
+
+// # Prime number within a given range: js | Python
+function prime_number(n) {
+  let i = 2
+  while (i <= n) {
+    let flag = true
+    let j = 2
+    while (j * j <= i) {
+      if (i % j == 0) {
+        flag = false
+        break
+      }
+      j += 1
+    }
+    if (flag) console.log(i, 'is a prime number')
+    i += 1
+  }
+}
+
+prime_number(15)
--- main.py
+++ main.py
@@ -42,10 +42,80 @@
 #     return sum
 # print(sumofNumber(2,5)) # 14
 # Greatest of two numbers: js | Python
+# def greatest_of_two(a,b):
+#     if a> b :
+#         return 'a is greater'
+#     else :
+#         return 'b is greater'
+
+# print(greatest_of_two(5,10))
 # Greatest of the Three numbers: js | Python
+# def greatest_of_three(a,b,c):
+#     if a> b and a> c :
+#         return 'a is greater'
+#     elif b> c and b> a :
+#         return 'b is greater'
+#     else :
+#         return 'c is greater'
+
+# print(greatest_of_three(5,10,12))
 # Leap year or not: js | Python
+# Divisible by 4 → If a year is divisible by 4, it is a leap year.
+# ✅ Example: 2024, 2028, 2032
+
+# Exception - Century Years → If a year is divisible by 100, it is not a leap year, unless it is also divisible by 400.
+# ❌ Example: 1700, 1800, 1900 (Not leap years)
+# ✅ Example: 1600, 2000 (Leap years)
+
+# def is_leap_year(year):
+#     if (year % 400==0)or (year % 4==0 and year % 100!=0):
+#         return f'{year} is a leap year'
+#     else :
+#         return f'{year} is not a leap year'
+        
+# print(is_leap_year(2018))
+
+
+
 # Prime number: js | Python
+# def prime_number(n):
+#     if n <= 1:
+#         return False
+#     i ,flag =2,True
+#     while i<n:
+#         if n%i==0:
+#             return False
+#         i+=1
+#     return flag
+# print(prime_number(12))
+# def prime_number(n):
+#     if n <= 1:
+#         return False
+#     i ,flag =2,True
+#     while i*i<n:
+#         print(i)
+#         if n%i==0:
+#             return False
+#         i+=1
+#     return flag
+# print(prime_number(15))
+
 # Prime number within a given range: js | Python
+# def prime_number(n):
+#         i = 2        
+#         while i<= n:
+#             flag = True
+#             j =2
+#             while j*j <=i:
+#                 if i % j ==0:
+#                     flag = False
+#                     break
+#                 j+=1
+#             if flag:
+#                 print(i,'is a prime number')
+#             i+=1
+
+# prime_number(15)
 # Sum of digits of a number: js | Python
 # Reverse of a number : js | Python
 # Palindrome number: js | Python
